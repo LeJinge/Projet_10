36f83928f39b2f885b5241ddd34abeb0fdfde2ea
LeJinge <jeremycarmona28@gmail.com>
2024-01-22 16:36:46 +0100

    Initialisation du projet

--- Projet_10/urls.py
+++ Projet_10/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('projects/', include('projects.urls')),
+    path('issues/', include('issues.urls')),
+    path('comments/', include('comments.urls')),
+    path('contributors/', include('contributors.urls')),
 ]
